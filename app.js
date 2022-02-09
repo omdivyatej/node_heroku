61148423211ed4be682d13ee925d06247d4faeab
omdivyatej <ohmbrock42@gmail.com>
2022-02-09 17:01:05 +0530

    ddnj

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 const WebSocket = require("ws");
 
 const PORT =process.env.PORT || 5000;
-
+var connections=[]
 const wsServer = new WebSocket.Server({
   port: PORT,
 });
-var input = 'w'
-wsServer.on("connection", function (socket,req) {
-  // Some feedback on the console
-  console.log("A client just connected");
-  const ip = req.socket.remoteAddress;
 
-  // Attach some behavior to the incoming socket
-  socket.on("message", function (msg) {
-    console.log("Received message from client: " + msg);
-    // socket.send("Take this back: " + msg);
-
-    // Broadcast that message to all connected clients
-    wsServer.clients.forEach(function (client) {
-      client.send("Your Ip " + ip);
+wsServer.on("connection", function (ws) {
+  
+  console.log("A client just connected");    
+   
+  ws.on("message", function (msg) {    
+    
+    console.log("Received message from client: " + msg);    
+    const obj = JSON.parse(msg)
+    
+    connections[obj.username] = ws
+    wsServer.clients.forEach(function (client) {      
+         
+    connections[obj.username].send("Hello from server: " +obj.username)
     });
+    
+    //connections[obj.username].send("Hello from server")
   });
 
-  socket.on("close", function () {
+  ws.on("close", function () {
     console.log("Client disconnected");
   });
 });
 
-console.log(new Date() + " Server is listening on port " + PORT);
+ console.log(new Date() + " Server is listening on port " + PORT);
+// var con={
+//   Name: "Eric",
+//   Age : 23,
+//   Job: "Freelancer",
+//   Skills : "JavaScript"
+// }
+// con["username"]="eric"
+// console.log(con)
+
